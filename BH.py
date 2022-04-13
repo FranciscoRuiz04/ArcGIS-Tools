f7d5f2e1180b9dacf7ba7ef039e73ac6ed895bd7
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-04-13 18:57:23 -0500

    Version 1.0.1 para calculo de k y BH

---
+++ BH.py
@@ -0,0 +1,166 @@
+__author__ = "Ulises Francisco Ruiz Gomez"
+__copyright__ = "Copyright 2022, GPS"
+__credits__ = "GPS"
+
+__version__ = "1.0.1"
+__maintainer__ = "Francisco Ruiz"
+__email__ = "uf.ruizgomez@ugto.mx"
+__status__ = "Developer"
+
+#+++++++++++++++++++++++++  LIBRARIES   +++++++++++++++++++++++++++++
+import os
+import pandas as pd
+import shapefile as shp
+import arcpy
+
+
+#+++++++++++++++++++++++++  FUNCTIONS   +++++++++++++++++++++++++++++
+def escorrentia(k, p, rd = False):
+    """
+    
+    Calculo de la cantidad de agua, en mm, que genera una corriente
+    superficial de acuerdo con la NOM-011-2015 de CONAGUA.
+    
+    <k>
+    Valor de k ponderado promedio.
+    
+    <p>
+    Precipiacion ponderada por área (mm).
+    
+    <rd>
+    Decimales a redondear.
+    
+    """
+    
+    if k <= 0.15: ce = k*(p-250)/2000
+    else: ce = k*(p-250)/2000 + (k-0.15)/1.5
+    
+    if rd != False: ce = round(ce, rd)
+    
+    return ce
+
+def et_turc(p, t, rd = False, percentage = False):
+    """
+    
+    Ecuación para el calculo de la evapotranspiracion real en milimetros,
+    tomando en cuenta la precipitacion y la temperatura de
+    la zona de estudio.
+    
+    <precip>
+    Altura de la lámina de precipitación anual en milimetros.
+    
+    <t>
+    Temperatura de la zona de estudio en grados celcius.
+    
+    <rd>
+    Decimales a redondear.
+    
+    <percentage>
+    Evapotranspiracion arrojada en porcentaje.
+    """
+    #Calculo del valor de L
+    l = 300 + 25*t + 0.05*pow(t, 3)
+    
+    #Calculo de la ET
+    etr = p/pow(0.9 + pow(p/l, 2), 1/2)
+    
+    if rd != False: etr = round(etr, rd)
+    
+    if percentage:
+        etr_per = round((p/etr)*100, 2)
+        return ("ET: {}, Porcentaje: {}%").format(etr, etr_per)
+    else: return etr
+
+def infil(p, et, esc):
+    """
+    
+    Calculo de la escorrentia
+    
+    <p>
+    Precipitación en mm
+    
+    <et>
+    Evapotranspiración en mm
+    
+    <esc>
+    Escurrimiento en mm
+    
+    """
+    
+    inf = p - (et + esc)
+    
+    return inf
+
+
+#+++++++++++++++++++++++++  PARAMETERS   +++++++++++++++++++++++++++++
+stations_file = arcpy.GetParameterAsText(0)
+output_path = arcpy.GetParameterAsText(1)
+experimental_k = arcpy.GetParameterAsText(2)
+experimental_tmp = arcpy.GetParameterAsText(3)
+
+
+#+++++++++++++++++++++++++  ARGUMENTS   +++++++++++++++++++++++++++++
+arcpy.env.overwriteOutput = False
+arcpy.env.workspace = output_path
+
+bh_file = output_path + os.sep + 'BH.csv'
+
+stations_shp = shp.Reader(stations_file)
+outcome_tab = pd.DataFrame()
+
+
+#+++++++++++++++++++++++++  PROCEDUREMENT  +++++++++++++++++++++++++++++
+atts = stations_shp.fields[1:]  #Fetch oficial fields only.
+colnames = []
+for r in atts:
+    colnames.append(r[0])   #Fetch attribute name only. Datatype, length, etc., are ignored.
+
+ptemp_tab = pd.DataFrame(data = stations_shp.records(), columns = colnames)
+
+
+arcpy.AddMessage('Calculated...')
+#------------------------    OUTCOME FILLING    --------------------------
+#-------------------------------------------------------------------------
+outcome_tab['ID_STATION'] = ptemp_tab['ID_ESTACIO']
+outcome_tab['POND_PRECIP'] = ptemp_tab['PRECIP'] * ptemp_tab['POLY_AREA']
+
+if experimental_tmp == '':
+    outcome_tab['TMP'] = ptemp_tab['TEMP']
+else:
+    outcome_tab['TMP'] = float(experimental_tmp)
+
+outcome_tab['POND_TMP'] = outcome_tab['TMP'] * ptemp_tab['POLY_AREA']
+outcome_tab['POND_TMP'] = outcome_tab['TMP'] * ptemp_tab['POLY_AREA']
+
+if experimental_k == '':
+    outcome_tab['K'] = ptemp_tab['K']
+else:
+    outcome_tab['K'] = float(experimental_k)
+
+
+#----------------    EFFECTIVE PRECIPITATION, ET & INFILTRATION    ---------------
+esc_values = []
+et_values = []
+infil_values = []
+i = 0
+while i < len(ptemp_tab.index):
+    esc = escorrentia(outcome_tab['K'].iloc[i], outcome_tab['POND_PRECIP'].iloc[i], 2)
+    et = et_turc(outcome_tab['POND_PRECIP'].iloc[i], outcome_tab['POND_TMP'].iloc[i], 2)
+    infil_value = infil(outcome_tab['POND_PRECIP'].iloc[i], et, esc)
+    esc_values.append(esc)
+    et_values.append(et)
+    infil_values.append(infil_value)
+    i += 1
+
+arcpy.AddMessage('Adding Fields...')
+outcome_tab['ESC'] = esc_values
+outcome_tab['ET'] = et_values
+outcome_tab['INF'] = infil_values
+
+
+arcpy.AddMessage('Exporting...')
+#-----------------------    EXPORT OUTCOME DF    -------------------------
+#-------------------------------------------------------------------------
+outcome_tab.to_csv(bh_file, encoding = 'utf-8')
+
+arcpy.AddMessage('Work Done!')
---
+++ SIG.py
@@ -0,0 +1,97 @@
+import os
+import pandas as pd
+import arcpy
+
+#Parameters
+tab_name = arcpy.GetParameterAsText(0)  #Table name
+statis_path = arcpy.GetParameterAsText(1)
+
+#Enviroment Arguments
+arcpy.env.overwriteOutput = False
+# arcpy.env.workspace = statis_path
+
+
+#Arguments
+complete_path = r"C:\Users\Francisco Ruiz\OneDrive - Universidad de Guanajuato\UG\SEM_6\SIG_AVANZADO_2022\SEMANA_08"
+ks_records = pd.read_csv(complete_path + os.sep + "GPS_COEFICIENTES.csv", encoding='utf-8')
+statis_path += os.sep + 'aux_Ks'
+
+arcpy.AddMessage("Creating aux_ks folder")
+#Create a new folder where will be saved whole created files
+if not os.path.isdir(statis_path):
+    os.mkdir(statis_path)
+statis_file = statis_path + os.sep + 'statistics.csv'
+
+arcpy.AddMessage("Creating summary")
+#Proccesses
+arcpy.Statistics_analysis(in_table=tab_name, out_table=statis_file, statistics_fields=[["US_EDAFO", "COUNT"]], case_field=["US_EDAFO"])
+
+
+#Get ks
+class Suelo:
+    def __init__(self, type, permea):
+        self.tipo = type
+        self.permea = permea
+    
+    def iguala(self):
+        if self.tipo == "Agricultura de riego (incluye riego eventual)" or self.tipo == "Agricultura de humedad":
+            suelo_k = "Cultivos En Hilera"
+        elif self.tipo == "Agricultura de temporal":
+            suelo_k = "Cultivos Legumbres o rotación de pradera"
+        elif self.tipo == "Área sin vegetación aparente":
+            suelo_k = "Barbecho, áreas incultas y desnudas"
+        elif self.tipo == "Asentamiento humano" or self.tipo == "Cuerpo de agua":
+            suelo_k = "Zonas urbanas"
+        elif self.tipo == "Bosque de encino":
+            suelo_k = "Bosque Cubierto del 50 al 75%"
+        elif self.tipo == "Bosque de pino" or self.tipo == "Bosque de pino-encino (incluye encino-pino)":
+            suelo_k = "Bosque Cubierto más del 75%"
+        elif self.tipo == "Chaparral":
+            suelo_k = "Pastizal Más del 75% - Poco -"
+        elif self.tipo == "Matorral crasicaule" or self.tipo == "Mezquital (incluye huizachal)" or self.tipo == "Pastizal natural (incluye pastizal - huizachal)":
+            suelo_k = r"Pastizal Menos del 50% - Excesivo -"
+        elif self.tipo == "Matorral subtropical" or self.tipo == "Pastizal inducido" or self.tipo == "Vegetación halófila y gipsófila":
+            suelo_k = "Pastizal Del 50 al 75% - Regular -"
+        
+        return suelo_k
+
+
+arcpy.AddMessage("Evaluating with basis on Criteria")
+#Evaluate US_EDAFO field with criteria
+gps_file = pd.DataFrame(columns=['ID', 'US_EDAFO', 'CRITERIA', 'K'])
+
+records = pd.read_csv(statis_file)
+records.pop('FID')
+records.pop('FREQUENCY')
+records.pop('COUNT_US_EDAFO')
+
+records["ID"] = records.index
+
+ks_records = pd.read_csv(complete_path + os.sep + "GPS_COEFICIENTES.csv", encoding = 'utf-8')
+
+
+for i,rec in records.iterrows():
+    #Aplicate criteria for ks
+    land_type = rec["US_EDAFO"].split("/")[0].strip()
+    land_permea = rec["US_EDAFO"].split("/")[1].strip()
+    land = Suelo(land_type, land_permea)
+    try:
+        patt = land.iguala() + " / " + land.permea
+    except:
+        arcpy.AddError(("There is not matched criteria for this land use: {}").format(rec["US_EDAFO"]))
+    else:
+        for n,r in ks_records.iterrows():
+            if r["Tipo"].strip() == patt.strip():
+                gps_file.loc[i] = [i, rec["US_EDAFO"], patt, r['k']]
+                break
+
+merged_left = pd.merge(left=records, right=gps_file, how='left', left_on='ID', right_on='ID')
+merged_left.pop('ID')
+merged_left.pop('US_EDAFO_y')
+
+merged_left.columns = merged_left.columns.str.replace('_x', '')
+
+
+merged_left.to_csv(statis_path + os.sep + 'k_values.csv', encoding='utf-8',index=False)
+arcpy.AddMessage("Evaluated")
+arcpy.AddMessage("Work Done")
