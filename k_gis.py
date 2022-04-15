438dced1f6a8456fce7c5f157789f9b66d246ac7
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-04-15 18:35:36 -0500

    Give format to code and rename parameter variables

--- k_gis.py
+++ k_gis.py
@@ -7,36 +7,41 @@ __maintainer__ = "Francisco Ruiz"
 __email__ = "franciscoruiz078@gmail.com"
 __status__ = "Developer"
 
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\     Libraries   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 import os
 import pandas as pd
 import arcpy
 
-#Parameters
-tab_name = arcpy.GetParameterAsText(0)  #Table name
-statis_path = arcpy.GetParameterAsText(1)
 
-#Enviroment Arguments
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\     Parameters   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+tab_name = arcpy.GetParameterAsText(0)  #Land shp file
+output_location = arcpy.GetParameterAsText(1)
+
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\     Enviroment Arguments   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 arcpy.env.overwriteOutput = False
-# arcpy.env.workspace = statis_path
 
 
-#Arguments
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\     Arguments   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 complete_path = r"C:\Users\Francisco Ruiz\OneDrive - Universidad de Guanajuato\UG\SEM_6\SIG_AVANZADO_2022\SEMANA_08"
 ks_records = pd.read_csv(complete_path + os.sep + "GPS_COEFICIENTES.csv", encoding='utf-8')
-statis_path += os.sep + 'aux_Ks'
+output_location += os.sep + 'aux_Ks'
 
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\     Procedurement   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 arcpy.AddMessage("Creating aux_ks folder")
 #Create a new folder where will be saved whole created files
-if not os.path.isdir(statis_path):
-    os.mkdir(statis_path)
-statis_file = statis_path + os.sep + 'statistics.csv'
+if not os.path.isdir(output_location):
+    os.mkdir(output_location)
+statis_file = output_location + os.sep + 'statistics.csv'
 
 arcpy.AddMessage("Creating summary")
-#Proccesses
+#++++++++++++++++++++++++++     Generate statistic file     +++++++++++++++++++++++
 arcpy.Statistics_analysis(in_table=tab_name, out_table=statis_file, statistics_fields=[["US_EDAFO", "COUNT"]], case_field=["US_EDAFO"])
 
 
-#Criteria
+#++++++++++++++++++++++++++     Criteria     +++++++++++++++++++++++
 criteria = {
     "Agricultura de temporal": "Cultivos Legumbres o rotación de pradera",
     "Área sin vegetación aparente": "Barbecho, áreas incultas y desnudas",
@@ -45,7 +50,7 @@ criteria = {
     }
 
 
-#Get ks
+#++++++++++++++++++++++++++     Get k coefficients     +++++++++++++++++++++++
 class Suelo:
     def __init__(self, type, permea):
         self.tipo = type
@@ -105,6 +110,7 @@ merged_left.pop('US_EDAFO_y')
 merged_left.columns = merged_left.columns.str.replace('_x', '')
 
 
-merged_left.to_csv(statis_path + os.sep + 'k_values.csv', encoding='utf-8',index=False)
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\     Create ks csv file   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+merged_left.to_csv(output_location + os.sep + 'k_values.csv', encoding='utf-8',index=False)
 arcpy.AddMessage("Evaluated")
 arcpy.AddMessage("Work Done")
